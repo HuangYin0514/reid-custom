b3ec9d94bbe3e39d85648c8a344006ba33d66e9f
Huang Yin <1071324110@qq.com>
2020-05-04 23:21:37 +0800

    fix bug

--- projects/PCB/run.sh
+++ projects/PCB/run.sh
@@ -10,3 +10,9 @@ python /kaggle/working/reid-custom/projects/PCB/main.py \
 python projects/PCB/main.py \
 --config-file  projects/PCB/pcb_p6_test.yaml \
 --model-resume /home/hy/vscode/reid-custom/log/pcb_p6_save/model/model.pth.tar-60
+
+# test kaggle
+!python projects/PCB/main.py \
+--config-file  projects/PCB/pcb_p6_test.yaml \
+--model-resume /kaggle/working/log/pcb_p6_save/model/model.pth.tar-60 \
+--root /kaggle/input/
