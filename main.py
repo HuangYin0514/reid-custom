9cb757a754bc564c16f28f7818f8a4f12a119de7
Huang Yin <1071324110@qq.com>
2020-05-22 23:23:17 +0800

    1

--- main.py
+++ main.py
@@ -11,50 +11,38 @@ from train import train
 
 parser = argparse.ArgumentParser(description='Person ReID Frame')
 
-"""
-System parameters
-"""
+
+# System parameters#System parameters-------------------------------------------------------------
 parser.add_argument('--nThread', type=int, default=4, help='number of threads for data loading')
 parser.add_argument('--nGPU', type=int, default=1, help='number of GPUs')
 parser.add_argument('--save_path', type=str, default='./experiments')
 parser.add_argument('--experiment', type=str, default='PCB_p6')
 
-
-"""
-Data parameters
-"""
+# Data parameters-------------------------------------------------------------
 parser.add_argument('--dataset', type=str, default='Market1501')
 parser.add_argument('--dataset_path', type=str, default='/home/hy/vscode/reid-custom/data/Market-1501-v15.09.15')
 parser.add_argument('--height', type=int, default=384, help='height of the input image')
 parser.add_argument('--width', type=int, default=128, help='width of the input image')
 parser.add_argument('--batch_size', default=64, type=int, help='batch_size')
 
-"""
-Model parameters
-"""
+# Model parameters-------------------------------------------------------------
 parser.add_argument('--share_conv', default=False, action='store_true')
 parser.add_argument('--stripes', type=int, default=6)
 parser.add_argument('--open_layers', nargs='+', default=[])
 
 
-"""
-Train parameters
-"""
+# Train parameters-------------------------------------------------------------
 parser.add_argument('--epochs', type=int, default=60)
 parser.add_argument('--test_every', type=int, default=10)
 parser.add_argument('--fixbase_epoch', type=int, default=0)
 
 
-"""
-Optimizer parameters
-"""
+# Optimizer parameters-------------------------------------------------------------
 parser.add_argument('--lr', type=float, default=0.1)
 parser.add_argument('--lr_base', type=float, default=0.01)
 
 
-"""
-Learning rate parameters
-"""
+# Learning rate parameters-------------------------------------------------------------
 parser.add_argument('--decay_every', type=int, default=20)
 parser.add_argument('--gamma', type=float, default=0.1)
 
