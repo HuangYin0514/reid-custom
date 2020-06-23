0c4953f766a28a16067ebaff94778f90c2015d99
Huang Yin <1071324110@qq.com>
2020-06-23 16:13:43 +0800

    pcb structure

--- models/pcb_init.py
+++ models/pcb_init.py
@@ -3,37 +3,103 @@ from torch import nn
 from torch.nn import functional as F
 from torchvision import models
 from utils import torchtool
+import torch.utils.model_zoo as model_zoo
+
+
+model_urls = {
+    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
+    'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
+    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
+    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
+}
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(
+            planes,
+            planes,
+            kernel_size=3,
+            stride=stride,
+            padding=1,
+            bias=False
+        )
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(
+            planes, planes * self.expansion, kernel_size=1, bias=False
+        )
+        self.bn3 = nn.BatchNorm2d(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
 
 
 class PCBModel(nn.Module):
-    def __init__(self, num_classes, num_stripes, share_conv, loss='softmax', ** kwargs):
+    def __init__(self,
+                 num_classes,
+                 block,
+                 layers,
+                 parts=6,
+                 reduced_dim=256,
+                 nonlinear='relu',
+                 ** kwargs):
 
         super(PCBModel, self).__init__()
-        self.num_stripes = num_stripes
-        self.num_classes = num_classes
-        self.loss = loss
+        #setting of parameters--------------------------------------------------------------------------
+        self.parts = parts
+        self.feature_dim = 512 * block.expansion
+        self.inplanes = 64
 
-        # bone--------------------------------------------------------------------------
-        resnet = models.resnet50(pretrained=True)
-        # Modifiy the stride of last conv layer----------------------------
-        resnet.layer4[0].downsample[0].stride = (1, 1)
-        resnet.layer4[0].conv2.stride = (1, 1)
-        # Remove avgpool and fc layer of resnet------------------------------
-        # modules = list(resnet.children())[:-2]
-        # self.backbone = nn.Sequential(*modules)
+        # backbone network--------------------------------------------------------------------------
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=1)
+        
         self.backbone = nn.Sequential(
-            resnet.conv1, resnet.bn1, resnet.relu, resnet.maxpool,
-            resnet.layer1, resnet.layer2, resnet.layer3, resnet.layer4)
-
+            self.conv1, self.bn1, self.relu, self.maxpool,
+            self.layer1, self.layer2, self.layer3, self.layer4)
         ####################################################################################
 
         # avgpool--------------------------------------------------------------------------
-        self.avgpool = nn.AdaptiveAvgPool2d((self.num_stripes, 1))
+        self.avgpool = nn.AdaptiveAvgPool2d((self.parts, 1))
         # self.dropout = nn.Dropout(p=0.5)
 
         # local_conv--------------------------------------------------------------------
         self.local_conv_list = nn.ModuleList()
-        for _ in range(num_stripes):
+        for _ in range(parts):
             local_conv = nn.Sequential(
                 nn.Conv1d(2048, 256, kernel_size=1),
                 nn.BatchNorm1d(256),
@@ -43,17 +109,39 @@ class PCBModel(nn.Module):
 
         # Classifier for each stripe--------------------------------------------------------------------------
         self.fc_list = nn.ModuleList()
-        for _ in range(num_stripes):
+        for _ in range(parts):
             fc = nn.Linear(256, num_classes)
-            # nn.init.normal_(fc.weight, std=0.001)
-            # nn.init.constant_(fc.bias, 0)
             fc.apply(torchtool.weights_init_classifier)
             self.fc_list.append(fc)
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(
+                    self.inplanes,
+                    planes * block.expansion,
+                    kernel_size=1,
+                    stride=stride,
+                    bias=False
+                ),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+        return nn.Sequential(*layers)
+
+    def featuremaps(self, x):
+        x = self.backbone(x)
+        return x
 
     def forward(self, x):
         # backbone------------------------------------------------------------------------------------
         # tensor T
-        resnet_features = self.backbone(x)
+        resnet_features = self.featuremaps(x)
 
         # tensor g---------------------------------------------------------------------------------
         # [N, C, H, W]
@@ -63,7 +151,7 @@ class PCBModel(nn.Module):
         # 1x1 conv---------------------------------------------------------------------------------
         # [N, C=256, H=S, W=1]
         features_H = []
-        for i in range(self.num_stripes):
+        for i in range(self.parts):
             stripe_features_H = self.local_conv_list[i](features_G[:, :, i, :])
             features_H.append(stripe_features_H)
 
@@ -76,15 +164,41 @@ class PCBModel(nn.Module):
         # fc---------------------------------------------------------------------------------
         # [N, C=num_classes]
         batch_size = x.size(0)
-        logits_list = [self.fc_list[i](features_H[i].view(batch_size, -1)) for i in range(self.num_stripes)]
+        logits_list = [self.fc_list[i](features_H[i].view(batch_size, -1)) for i in range(self.parts)]
 
         return logits_list
 
 
-def PCB_init(num_classes, num_stripes=6, **kwargs):
+def init_pretrained_weights(model, model_url):
+    """Initializes model with pretrained weights.
+
+    Layers that don't match with pretrained layers in name or size are kept unchanged.
+    """
+    pretrain_dict = model_zoo.load_url(model_url)
+    model_dict = model.state_dict()
+    pretrain_dict = {
+        k: v
+        for k, v in pretrain_dict.items()
+        if k in model_dict and model_dict[k].size() == v.size()
+    }
+    # print(pretrain_dict.keys())
+    model_dict.update(pretrain_dict)
+    model.load_state_dict(model_dict)
+
+
+def PCB_init(num_classes, num_stripes=6, pretrained=True, **kwargs):
     assert num_stripes == 6, "num_stripes not eq 6"
-    return PCBModel(
+
+    model = PCBModel(
         num_classes=num_classes,
-        num_stripes=num_stripes,
+        block=Bottleneck,
+        layers=[3, 4, 6, 3],
+        parts=num_stripes,
+        reduced_dim=256,
+        nonlinear='relu',
         **kwargs
     )
+    if pretrained:
+        init_pretrained_weights(model, model_urls['resnet50'])
+
+    return model
--- test_module/test_model.py
+++ test_module/test_model.py
@@ -1,19 +1,20 @@
-
 import sys
 sys.path.append('/home/hy/vscode/reid-custom')
 print(sys.path)
-from models import build_model
+from torchsummary import summary
 import torch
+from models import build_model
 
 if __name__ == "__main__":
 
-    model = build_model('PCB_p6', num_classes=64, share_conv=True)
-    print(model)
+    model = build_model('PCB_init', num_classes=64, share_conv=True)
 
     model.eval()
-    inp1 = torch.randn(3,3,384,128)
+    inp1 = torch.randn(3, 3, 384, 128)
     out = model(inp1)
-    print([out_item.shape for out_item in out])
 
+    # print(model)
+    # print([out_item.shape for out_item in out])
+    # summary(model, (3, 64, 64))
 
     print('complete check.')
