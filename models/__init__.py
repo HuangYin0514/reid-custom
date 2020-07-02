7671f8c4bec2b7e913f77c631402ea68160fd8dd
Huang Yin <1071324110@qq.com>
2020-07-02 12:20:56 +0800

    1

--- models/__init__.py
+++ models/__init__.py
@@ -5,20 +5,22 @@ from .pcb import PCB_p6
 from .resnet import Res_net
 from .resnet_att import Resnet_self_attention
 from .pcb_init import PCB_init
+from .rga import rga_init
 
 
 __model_factory = {
     'PCB_p6': PCB_p6,
     'Res_net': Res_net,
     'Resnet_self_attention': Resnet_self_attention,
-    'PCB_init': PCB_init
+    'PCB_init': PCB_init,
+    'rga_init': rga_init
 }
 
 
-def build_model(name, num_classes, **kwargs):
+def build_model(name, **kwargs):
     avai_models = list(__model_factory.keys())
     if name not in avai_models:
         raise KeyError(
             'Unknown model: {}. Must be one of {}'.format(name, avai_models)
         )
-    return __model_factory[name](num_classes=num_classes, **kwargs)
+    return __model_factory[name](**kwargs)
---
+++ test_module/test_rga.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append('/home/hy/vscode/reid-custom')
+print(sys.path)
+from models import build_model
+import torch
+from torchsummary import summary
+
+if __name__ == "__main__":
+
+    model = build_model('rga_init')
+
+    model.eval()
+    inp1 = torch.randn(3, 3, 384, 128)
+    out = model(inp1)
+
+    # print(model)
+    # print([out_item.shape for out_item in out])
+    summary(model, (3, 64, 64))
+
+    print('complete check.')
