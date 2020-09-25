4be398b6188eb27d459a54e316700eda4a925a46
Huang Yin <1071324110@qq.com>
2020-09-25 10:23:21 +0800

    1

--- utils/util.py
+++ utils/util.py
@@ -1,3 +1,4 @@
+import warnings
 import random
 import numpy as np
 import matplotlib.pyplot as plt
@@ -136,59 +137,52 @@ def save_network(network, path, epoch_label):
 
 
 def load_network(network, path, epoch_label):
+    # devie---------------------------------------------------------------------------
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # file name -----------------------------------------------------------------------------
     file_path = os.path.join(path, 'net_%s.pth' % epoch_label)
 
-    # Original saved file with DataParallel
-    state_dict = torch.load(file_path, map_location=lambda storage, loc: storage)
+    # Original saved file with DataParallel-------------------------------------------------------
+    state_dict = torch.load(file_path, map_location=torch.device(device))
 
-    # If the model saved with DataParallel, the keys in state_dict contains 'module'
-    if list(state_dict.keys())[0][:6] == 'module':
-        # Create new OrderedDict that does not contain `module.`
-        new_state_dict = OrderedDict()
-        for k, v in state_dict.items():
-            key_name = k[7:]  # remove `module.`
-            new_state_dict[key_name] = v
+    # state dict--------------------------------------------------------------------------
+    model_dict = network.state_dict()
+    new_state_dict = OrderedDict()
+    matched_layers, discarded_layers = [], []
 
-        state_dict = new_state_dict
+    # load model state ---->{matched_layers, discarded_layers}------------------------------------
+    for k, v in state_dict.items():
+        if k.startswith('module.'):
+            k = k[7:]  # discard module.
 
-    # # ------------- PCB specific -------------
-    # # Load PCB from another dataset, change the fc_list parameters' shape
-    for name in state_dict.keys():
-        if name[0:7] == 'fc_list' or name[0:14]=='global_softmax':
-            desired_shape = network.state_dict()[name].shape
-            if desired_shape != state_dict[name].shape:
-                state_dict[name] = torch.randn(desired_shape)
-    # # ------------------------------------------------
+        if k in model_dict and model_dict[k].size() == v.size():
+            new_state_dict[k] = v
+            matched_layers.append(k)
+        else:
+            discarded_layers.append(k)
 
-    network.load_state_dict(state_dict)
+    model_dict.update(new_state_dict)
+    network.load_state_dict(model_dict)
+
+    # assert model state ------------------------------------------------------------------------
+    if len(matched_layers) == 0:
+        warnings.warn(
+            'The pretrained weights "{}" cannot be loaded, '
+            'please check the key names manually '
+            '(** ignored and continue **)'.format(path)
+        )
+    else:
+        print(
+            'Successfully loaded pretrained weights from "{}"'.
+            format(path)
+        )
+        if len(discarded_layers) > 0:
+            print(
+                '** The following layers are discarded '
+                'due to unmatched keys or layer size: {}'.
+                format(discarded_layers)
+            )
 
     return network
 
-
-if __name__ == "__main__":
-    # Logger instance
-    logger = Logger('./experiments/')
-    logger.info('-' * 10)
-    # logger.info(vars(arg))
-    num_epochs = 60
-    for epoch in range(num_epochs):
-        epoch_loss = epoch*np.random.rand(1)[0]
-        logger.info('Training Loss: {:.4f}'.format(epoch_loss))
-        # Save result to logger
-        logger.x_epoch_loss.append(epoch + 1)
-        logger.y_train_loss.append(epoch_loss)
-        if (epoch + 1) % 10 == 0 or epoch + 1 == num_epochs:
-            # Testing / Validating
-            CMC = epoch*np.random.rand(10)*10
-            mAP = epoch*np.random.rand(1)*10
-            logger.info('Testing: top1:%.2f top5:%.2f top10:%.2f mAP:%.2f' %
-                        (CMC[0], CMC[4], CMC[9], mAP))
-
-            logger.x_epoch_test.append(epoch + 1)
-            logger.y_test['top1'].append(CMC[0])
-            logger.y_test['mAP'].append(mAP)
-
-        logger.info('-' * 10)
-
-    # Save the loss curve
-    logger.save_curve()
