4cef9a83c81c29327f43066a024a364f5cba1b8c
Huang Yin <1071324110@qq.com>
2020-10-08 16:47:46 +0800

    1

--- main.py
+++ main.py
@@ -19,39 +19,6 @@ parser.add_argument('--nGPU', type=int, default=1, help='number of GPUs')
 parser.add_argument('--save_path', type=str, default='../experiments')
 parser.add_argument('--experiment', type=str, default='resnet50_cbam_reid_model_v4')
 
-# Data parameters-------------------------------------------------------------
-parser.add_argument('--dataset_name', type=str, default='market1501')
-parser.add_argument('--dataset_path', type=str, default='/home/hy/vscode/data/Market-1501-v15.09.15')
-parser.add_argument('--img_height', type=int, default=384, help='height of the input image')
-parser.add_argument('--img_width', type=int, default=128, help='width of the input image')
-parser.add_argument('--batch_size', default=6, type=int, help='batch_size')
-parser.add_argument('--test_batch_size', default=6, type=int, help='test_batch_size')
-parser.add_argument('--data_sampler_type', type=str, default='RandomIdentitySampler')
-parser.add_argument('--num_instance', type=int, default=2)
-
-
-# Model parameters-------------------------------------------------------------
-parser.add_argument('--stripes', type=int, default=6)
-
-
-# Train parameters-------------------------------------------------------------
-parser.add_argument('--epochs', type=int, default=2)
-parser.add_argument('--test_every', type=int, default=2)
-parser.add_argument('--fixbase_epoch', type=int, default=0)
-
-
-# Optimizer parameters-------------------------------------------------------------
-parser.add_argument('--lr', type=float, default=0.1)
-
-
-# Learning rate parameters-------------------------------------------------------------
-parser.add_argument('--decay_every', type=int, default=20)
-parser.add_argument('--gamma', type=float, default=0.1)
-
-# test other datset parameters-------------------------------------------------------------
-parser.add_argument('--test_other_dataset_name', type=str, default='Occluded_REID')
-parser.add_argument('--test_other_dataset_path', type=str, default='/home/hy/vscode/data/Occluded_REID')
-
 args = parser.parse_args()
 
 
