ec0516a80994cd6c73b9ef06e1f1f4f78b505926
Huang Yin <1071324110@qq.com>
2020-11-21 12:30:09 +0800

    1

--- main.py
+++ main.py
@@ -63,13 +63,13 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Fix random seed---------------------------------------------------------------------------
-    def set_seed(seed):
-        np.random.seed(seed)
-        random.seed(seed)
-        torch.manual_seed(seed)  # cpu
-        torch.cuda.manual_seed_all(seed)  # gpu
-        torch.backends.cudnn.deterministic = True
-    set_seed(1)
+    # def set_seed(seed):
+    #     np.random.seed(seed)
+    #     random.seed(seed)
+    #     torch.manual_seed(seed)  # cpu
+    #     torch.cuda.manual_seed_all(seed)  # gpu
+    #     torch.backends.cudnn.deterministic = True
+    # set_seed(1)
 
     # speed up compution---------------------------------------------------------------------------
     cudnn.benchmark = True
